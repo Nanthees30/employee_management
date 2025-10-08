82ee9536867fa8131a9e2873ac3aaaeaf042b6bb
Nanthees <nanthees30@gmail.com>
2025-10-08 14:00:41 +0530

    Final Code

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,11 @@ const fs = require("fs");
 const mysql = require("mysql2");
 
 const app = express();
-app.use(cors());
+app.use(cors(
+  {
+    origin: "http://localhost:5173/"
+  }
+));
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
